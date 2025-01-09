4b3875443e64424787699061c4b015cd93129895
Satyam Bheel <136482276+satyam-bheel1999@users.noreply.github.com>
2025-01-10 01:19:38 +0530

    progress

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -11,52 +11,64 @@ function Todo() {
     e.preventDefault();
 
     if (input.trim()) {
-      setArray([...array, input]);
+      setArray([...array, { text: input, completed: false }]);
     }
 
     setInput("");
   };
 
-  // const HandleComplete = (indexToComplete) =>{
-  //   setArray((prevArray) => prevArray.map((index) => index == indexToComplete ?  ))
-  // }
+  const HandleComplete = (indexToComplete) => {
+    setArray((prevArray) =>
+      prevArray.map((item, index) =>
+        index == indexToComplete ? { ...item, completed: true } : item
+      )
+    );
+  };
 
-  const HandleRemove = (indexToRemove) =>{
-
-    setArray((prevArray) => prevArray.filter((_, index) => index !== indexToRemove))
-  }
-
-
-
+  const HandleRemove = (indexToRemove) => {
+    setArray((prevArray) =>
+      prevArray.filter((_, index) => index !== indexToRemove)
+    );
+  };
 
   return (
     <>
-      <form>
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => {
-            setInput(e.target.value);
-          }}
-          placeholder="Enter your Task"
-        ></input>
+      <div className="h-2/3 w-80 border rounded-md shadow-xl relative top-8 right-36 p-7 bg-green-300 ">
+        <form className="flex flex-col">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+            placeholder="Enter your Task"
+          ></input>
 
-        <button onClick={HandleSubmit}>Add</button>
-      </form>
+          <button onClick={HandleSubmit} className="relative top-8 border border-yellow-300 rounded-3xl p-2 text-base text-white font-bold">Add</button>
+        </form>
+      </div>
 
-      <ul>
-        {array.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => HandleComplete(index)}>
-              <FontAwesomeIcon icon={faCheck} />
-            </button>
-            <button onClick={()=> HandleRemove(index)}>
-              <FontAwesomeIcon icon={faCircleXmark} />
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div className="relative top-8 left-32 h-96 w-80 ">
+        <h1 className="text-center font-extrabold text-2xl">Your To-Do</h1>
+        <ul className="p-5">
+          {array.map((item, index) => (
+            <li
+              style={{
+                textDecoration: item.completed ? "line-through" : "none",
+              }}
+              key={index} className="flex justify-between" 
+            >
+              {item.text}
+              <button onClick={() => HandleComplete(index)}>
+                <FontAwesomeIcon icon={faCheck} />
+              </button>
+              <button onClick={() => HandleRemove(index)}>
+                <FontAwesomeIcon icon={faCircleXmark} />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
